0a5536275f5b5708f6f8aa34e337dea0d1dce6e6
fernandot10 <113951371+fernandot10@users.noreply.github.com>
2023-04-07 21:29:38 -0400

    coding quiz reupload

--- Assets/script.js
+++ Assets/script.js
@@ -1,212 +1,179 @@
-var index = 0;
-var score = 0;
-var highScores = []
-var selectionNumber
-var list = document.getElementById('highScores');
-var resetScreen = document.getElementById('reset-screen');
+let score = 0;
+var start = document.getElementById("startBtn");
+var submit = document.getElementById("submitBtn");
+start.onclick = startQuiz;
+submit.onclick = submitScore;
+var userScore = document.getElementById("userScore");
+
+var highscores = [];
 var timer = document.getElementById("timer");
+var initialsSection = document.getElementById("initialsSection");
+var initials = document.getElementById("initials");
+var scores = document.getElementById("scores");
 
-
-var instructions = document.getElementById("intro");
-var startScreen = document.getElementById('start-screen');
-var start = document.getElementById("start");
-var viewHighscores = document.getElementById("view-highscores");
-
-
-var questionScreen = document.getElementById('question-screen');
-var answerOutput = document.getElementById("answer-output");
-
-
-var endScreen = document.getElementById('end-screen');
-
-
-questionScreen.style.display = "none";
-endScreen.style.display = "none";
-resetScreen.style.display = "none";
-timer.style.display = "block";
-
-
-function startQuiz() {
-    startScreen.style.display = "none";
-    questionScreen.style.display = "block";
-    quizQuestions()
-    updateCountdown()
-}
-
-
-function quizQuestions() {
-    if (index < questions.length) {
-        var question = document.getElementById("question")
-        var btn1 = document.getElementById("btn-a")
-        var btn2 = document.getElementById("btn-b")
-        var btn3 = document.getElementById("btn-c")
-        var btn4 = document.getElementById("btn-d")
-
-
-        question.innerHTML = questions[index].question;
-        btn1.innerHTML = questions[index].choices[0];
-        btn2.innerHTML = questions[index].choices[1];
-        btn3.innerHTML = questions[index].choices[2];
-        btn4.innerHTML = questions[index].choices[3];
-    } else {
-        questionScreen.style.display = "none";
-        endScreen.style.display = "block";
-        reset()
-    }
-}
+var questionSection = document.getElementById("questionSection");
+var titleQ = document.getElementById("titleQ");
+var resultQ = document.getElementById("resultQ");
+var choices = document.getElementById("choices");
+var currentQ;
+var index = 0;
 
 
 var questions = [
     {
-        question: 'What does the abbreviation HTML stand for?',
+        title: 'What does the abbreviation HTML stand for?',
         choices:
             ['HyperText Markup Language',
                 'HighText Markup Language',
                 'HyperText Markdown Language',
                 'None of the above'],
-        answer: 0
+        answer: "HyperText Markup Language"
     },
     {
-        question: "How many sizes of headers are available in HTML by default?",
+        title: "How many sizes of headers are available in HTML by default?",
         choices: ['1',
-                  '2', 
-                  '5', 
-                  '6',],
-        answer: 3
+            '2',
+            '5',
+            '6',],
+        answer: "6"
     },
     {
-        question: "What is the smallest header in HTML by default?",
+        title: "What is the smallest header in HTML by default?",
         choices: ['h1', 'h3', 'h5', 'h6'],
-        answer: 3
+        answer: "h6"
     },
     {
-        question: "What are the types of lists available in HTML?",
+        title: "What are the types of lists available in HTML?",
         choices: ['Ordered, Unordered Lists',
             'Bulleted, Numbered Lists',
             'Named, Unnamed Lists',
             'None of the above'],
-        answer: 0
+        answer: "Ordered, Unordered Lists"
     },
     {
-        question: 'CSS stands for-',
+        title: 'CSS stands for-',
         choices: ['Cascade style sheets',
-                  'Color and style sheets',
-                  'Control style sheets'],
-        answer: 0
+            'Color and style sheets',
+            'Control style sheets',
+            'None of the above'],
+        answer: "Cascade style sheets"
     },
     {
-        question: 'Which function is used to serialize an object into a JSON string in Javascript?',
+        title: 'Which function is used to serialize an object into a JSON string in Javascript?',
         choices: ['stringify',
-                  'parse()',
-                  'convert()',
-                  'None of the above'],
-        answer: 0
+            'parse()',
+            'convert()',
+            'None of the above'],
+        answer: "stringify"
     },
     {
-        question: 'How do you select elements with the class name "example"?',
+        title: 'How do you select elements with the class name "example"?',
         choices: ['example',
-                  'example',
-                  '.example',
-                  'Class example'],
-        answer: 2
+            'example',
+            '.example',
+            'Class example'],
+        answer: ".example"
     },
     {
-        question: 'Which of the following is the correct syntax to select all paragraph elements in a div element?',
+        title: 'Which of the following is the correct syntax to select all paragraph elements in a div element?',
         choices: ['div p',
-                  'p',
-                  'div#p',
-                  'div~p'],
-        answer: 0
+            'p',
+            'div#p',
+            'div~p'],
+        answer: "div p"
     }
 ];
 
 
-let startingTime = 60;
-function updateCountdown() {
+function showQuestions() {
+    currentQ = questions[index];
+    titleQ.textContent = currentQ.title;
+    choices.innerHTML = "";
+
+    for (var i = 0; i < currentQ.choices.length; i++) {
+        var choicesBtn = Object.assign(document.createElement("button"), { id: "choiceBtn" })
+        choicesBtn.textContent = i + 1 + ". " + currentQ.choices[i];
+        choices.appendChild(choicesBtn);
+    }
+};
+
+
+let time = 60;
+function setTimer() {
     let countdownInterval = setInterval(function () {
-        if (startingTime >= 1) {
-            timer.textContent = startingTime;
-            startingTime--;
-        } else {
-            timer.textContent = "Time's up!";
-            clearInterval(countdownInterval)
-            onSubmit()
-            endQuiz()
+        time--;
+        timer.textContent = time + "";
+        if (time <= -1) {
+            pausedTimer(countdownInterval);
+            end();
+            time++;
+            timer.textContent = time + "";
         }
-    }, 1000)
-}
+    }, 1000);
+};
 
 
-function reset() {
-    startingTime = 60;
-}
-
-
-function quizSelection(selection) {
+choices.addEventListener("click", function (event) {
+    var selection = event.target;
     if (selection === questions[index].answer) {
-        answerOutput.innerHTML = "Correct!"
-        score += 10
+        var result;
+        score = score + 10;
     } else {
-        answerOutput.innerHTML = "Wrong!"
-        startingTime -= 5;
+        time -= 10;
     }
     index++;
+
     if (timer <= 0 || questions[index] === questions.length) {
-        endQuiz()
+        end()
     } else {
-        quizQuestions();
+        showQuestions();
     }
-}
+});
 
 
-function endQuiz() {
-    timer.style.display = "none";
-    questionScreen.style.display = "none";
-    resetScreen.style.display = "none";
-    endScreen.style.display = "block";
-}
+function startQuiz() {
+    setTimer();
+    start.setAttribute("hidden", "true");
+    timer.removeAttribute("hidden");
+
+    showQuestions();
+};
 
 
-function onSubmit() {
-    var initials = document.getElementById("initials").value;
-    highScores.push({ initials, score })
-    var listItem = document.createElement("LI");
-    listItem.appendChild(document.createTextNode(`${initials} - ${score}`));
-    list.appendChild(listItem);
-    score = 0;
-    endScreen.style.display = "none";
-    viewHighscores.style.display = "none";
-    resetScreen.style.display = "block";
-}
-
-
-function viewScores() {
-    if (startScreen.style.display === "block") {
-        startScreen.style.display = "none"
-    } else if (questionScreen.style.display === "block") {
-        questionScreen.style.display = "none"
-    } else if (endScreen.style.display === "block") {
-        endScreen.style.display = "none"
-    }
-    resetScreen.style.display = "block";
-    viewHighscores.style.display = "none";
-    timer.style.display = "none";
-    startScreen.style.display = "none";
-}
-
-
-function clearHighScores() {
-    highScores = []
-    list.innerHTML = "";
-}
-
-
-function restartQuiz() {
-    resetScreen.style.display = "none";
-    startScreen.style.display = "block";
-    index = 0;
+function end() {
+    pausedTimer();
+    document.getElementById("questionSection").setAttribute("hidden", "true");
+    initialsSection.removeAttribute("hidden")
+    submit.removeAttribute("hidden");
     answerOutput.innerHTML = "";
-    viewHighscores.style.display = "block";
-    timer.style.display = "block";
-    reset()
-}
+    resultQ.textContent = "Submit you score below";
+};
+
+
+function submitScore() {
+    initialsSection.removeAttribute("hidden");
+    submit.setAttribute("hidden", "true");
+    pausedTimer();
+
+};
+
+
+document.getElementById("submitBtn").addEventListener("click", function (event) {
+    event.preventDefault();
+    var userInitials = document.getElementById("initials").value;
+    initials.value = " ";
+    var userScore = userInitials.concat(": ", score);
+    highscores.push(userScore);
+    localStorage.setItem("highscores", JSON.stringify(highscores));
+    var listItem = document.createElement("li");
+    listItem.textContent = userScore;
+    scores.appendChild(listItem);
+})
+
+
+function viewHighscores() {
+    highscores = JSON.parse(localStorage.getItem("highscores"));
+    if (highscores === null) {
+        highscores = [];
+    }
+};
